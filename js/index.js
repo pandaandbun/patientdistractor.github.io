9f7e678503b656e3c21be1d8468741e1ce6c31bd
katanaWieldingPanda <Rico0Sandard@gmail.com>
2019-02-10 06:22:44 -0500

    switching master and doctor-patient

---
+++ js/index.js
@@ -0,0 +1,12 @@
+$("#doc").click(function(){
+	window.location = "doc.html";
+});
+
+$("#pat").click(function(){
+	window.location = "patient.html";
+});
+
+$("#patbutton").click(function(){
+	window.location = "./webvr/index.html";
+});
+
